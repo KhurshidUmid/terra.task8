f3af1b89368afa9e8c9bed3b9d611caf58eadd87
hurshid3170 <hurshid3170@gmail.com>
2025-10-23 17:05:49 +0000

    updated used_data file

--- user_data.sh
+++ user_data.sh
@@ -7,12 +7,25 @@ yum install -y httpd aws-cli jq
 systemctl enable httpd
 systemctl start httpd
 
-# Get instance metadata using IMDSv2
-TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)
-PRIVATE_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/local-ipv4)
+# Get instance metadata using IMDSv2 (simplified)
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/instance-id)
+
+PRIVATE_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/local-ipv4)
 
 # Create the simple web page
 cat > /var/www/html/index.html <<EOF
-This message was generated on instance $INSTANCE_ID with the following IP: $PRIVATE_IP
+<html>
+  <body>
+    <h1>This message was generated on instance $INSTANCE_ID</h1>
+    <p>Private IP: $PRIVATE_IP</p>
+  </body>
+</html>
 EOF
+
+# Restart the service to make sure page is served
+systemctl restart httpd
